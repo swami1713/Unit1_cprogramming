1caf566d942d64cc4c1669b948c9c5d57eb64df8
swami1713 <swamisawant45@gmail.com>
2026-09-21 12:44:10 +0530

    Create Average oj three numbers.c

---
+++ Average oj three numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+float a,b,c,average;
+printf("enter three numbers:");
+scanf("%f%f%f",&a,&b,&c);
+average=(a+b+c)/3;
+printf("average=%2f\n",average);
+return 0;
+}
